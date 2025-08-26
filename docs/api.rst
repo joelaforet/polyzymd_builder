716abb133a4104e1aaf56e5fac99215513a5e1ee
Joseph Laforet <jola3134@c3cpu-c15-u34-3.rc.int.colorado.edu>
2025-08-26 16:17:52 -0600

    added files

---
+++ docs/api.rst
@@ -0,0 +1,7 @@
+API Documentation
+=================
+
+.. autosummary::
+   :toctree: autosummary
+
+   polyzymd_builder.canvas
---
+++ polyzymd_builder/__init__.py
@@ -0,0 +1,7 @@
+"""Useful collection of scripts for the building of polymer-protein systems for MD simulations."""
+
+# Add imports here
+from .polymer_building import *
+
+
+from ._version import __version__
